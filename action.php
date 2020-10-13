e68a8581fcb7c6c39fa960a169892aa400ae2cc2
MANGEMANGE23 <43251887+MANGEMANGE23@users.noreply.github.com>
2020-10-13 12:56:12 -0500

    Update action.php

--- action.php
+++ action.php
@@ -1,9 +1,18 @@
-<?php
- $path = 'terrorists.txt';
- if (isset($_POST['name']) && isset($_POST['code'])) {
-    $fh = fopen($path,"a+");
-    $string = $_POST['field1'].' - '.$_POST['field2'];
-    fwrite($fh,$string); // Write information to the file
-    fclose($fh); // Close the file
- }
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your code name is <?php echo $_POST["code"]; ?>
+ <?php
+ //$path = 'terrorists.txt';
+// if (isset($_POST['name']) && isset($_POST['code'])) {
+ //   $fh = fopen($path,"a+");
+ //   $string = $_POST['field1'].' - '.$_POST['field2'];
+  //  fwrite($fh,$string); // Write information to the file
+ //   fclose($fh); // Close the file
+// }
+
 ?>
+
+</body>
+</html>
